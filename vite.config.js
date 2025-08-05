18946f5505b1c472293313210d9aa8f9326c543e
nyl20 <nicole.yliao@gmail.com>
2025-08-05 16:09:40 -0400

    Updating link

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/nicole_liao/",
+  // base: "/nicole_liao/",
   plugins: [
     vue(),
     vueDevTools(),
